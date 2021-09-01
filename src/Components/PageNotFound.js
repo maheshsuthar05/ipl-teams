2b96d34bf12b3086563e17fe1e684960391d895c
maheshsuthar05 <maheshsuthar051196@gmail.com>
2021-09-01 14:13:05 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/Components/PageNotFound.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PageNotFound extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>
+            <h1>Page not found. Please go to our homepage to explore our site.</h1>
+        </div> );
+    }
+}
+ 
+export default PageNotFound;
